c2f31898b67fc17763d17ac3f28e7d69bcd39a0d
Adekilekun Abdullahi <106445363+adekilekunabdullahi@users.noreply.github.com>
2023-03-13 11:41:19 +0100

    Update Validate_voters.php

--- Validate_voters.php
+++ Validate_voters.php
@@ -6,19 +6,19 @@ function validate_voter($age, $has_PVC, $ward){
 	if ($age >=18 && $has_PVC == true && $ward == 020){
 		echo "Voter eligible to vote";
 		}
-	if ($age < 18){
-		echo "Not Eligible: your age must be 18 or greater to vote <br>";
-		}
-	if ($has_PVC == false){
-		echo "Not Eligible: you must have pvc to vote <br>";
-		}
-	if ($ward != 020){
-		echo "Not Eligible: you must belong to ward 020 to vote <br>";
-		}
 	else{
-		echo "you are not eligible to vote";
+		echo "Not Eligible because: <br>";
+	        if ($age < 18){
+                        echo "your age must be 18 or greater to vote. <br>";
+                        }
+	        if ($has_PVC == false){
+		        echo "you must have pvc to vote. <br>";
 		}
+	        if ($ward != 020){
+		        echo "you must belong to ward 020 to vote <br>";
+		}
+	}
 }
-validate_voter(18, true, 020) //success
-validate_voter(17, true, 020) //failure
+validate_voter(18, true, 020); //success
+validate_voter(17, true, 020); //failure
 ?>
